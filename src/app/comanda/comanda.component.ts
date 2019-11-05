b8aa1777e384eb813ff01e175be76011ffe73a81
rimobile1 <icaro.mo1234@gmail.com>
2019-11-04 23:12:10 -0200

    exercicio 7-8

--- src/app/comanda/comanda.component.ts
+++ src/app/comanda/comanda.component.ts
@@ -14,6 +14,45 @@ export class ComandaComponent implements OnInit {
   constructor(public formBuilder: FormBuilder) { }
 
   ngOnInit() {
+
+    this.formComanda = this.formBuilder.group({
+      chopp : this.formBuilder.control(''),
+      Pizza : this.formBuilder.control(''),
+      recheio : this.formBuilder.control(''),
+      pessoa : this.formBuilder.control('')
+    })
+
+  }
+ onProcessar(){
+   
+  let totalChopp:number =   this.formComanda.value.chopp * 7.30;
+  let totalPizza:number =   this.formComanda.value.pizza * 31.50;
+  let totalRecheio:number =   this.formComanda.value.recheio * 5.90;
+  let totalPessoa:number =   this.formComanda.value.pessoa;
+  let valorTaxa: number = 0;   
+
+  let totalTotal: number = totalChopp + totalPizza + totalRecheio;
+
+  let resulTaxa: boolean = this.formComanda.value.taxaservico;
+
+  if(resulTaxa == true)
+  {
+      valorTaxa = totalTotal * 0.1;        
+      
+  }
+  else
+  {
+    valorTaxa = 0;
+    
+  }
+
+
+  let totalAPagar: number = totalTotal + valorTaxa;
+  let porPessoa: number = totalAPagar / totalPessoa;
+
+  alert(`Total: ${totalTotal} \n Valor Taxa: ${valorTaxa} \nTotal com Taxa: ${totalAPagar} \nTotal Por pessoa: ${porPessoa}`);
+
+
   }
 
 }
